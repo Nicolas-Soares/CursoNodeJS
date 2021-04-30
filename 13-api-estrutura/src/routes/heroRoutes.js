576c42adc7cc916b4079e2d3463bf8854d5f8252
Nicolas Conterato Soares <nicksoares3658@gmail.com>
2021-04-30 17:18:18 -0300

    Realização de testes
    Testes de filtro via URL

--- 13-api-estrutura/src/routes/heroRoutes.js
+++ 13-api-estrutura/src/routes/heroRoutes.js
@@ -11,7 +11,27 @@ class HeroRoutes extends BaseRoute{
             path: '/herois',
             method: 'GET',
             handler: (request, headers) => {
-                return this.db.read()
+                try {
+                    const { skip, limit, nome } = request.query
+
+                    let query = {}
+                    if (nome) {
+                        query.nome = nome
+                    }
+
+                    if (isNaN(skip)) {
+                        throw Error('O tipo de SKIP é incorreto')
+                    }
+                    if (isNaN(limit)) {
+                        throw Error('O tipo de LIMIT é incorreto')
+                    }
+
+                    return this.db.read(query, parseInt(skip), parseInt(limit))
+                } catch (error) {
+                    console.log('Deu ruim: ', error)
+                    return 'Erro interno no servidor'
+                }
+                
             }
         }
     }
--- 13-api-estrutura/src/tests/apiHeroes.test.js
+++ 13-api-estrutura/src/tests/apiHeroes.test.js
@@ -11,7 +11,7 @@ describe('Suite de testes na API Heroes', function() {
     it('Listar /herois', async () => {
         const result = await app.inject({
             method: 'GET',
-            url: '/herois'
+            url: '/herois?skip=0&limit=10'
         })
 
         const dados = JSON.parse(result.payload)
@@ -19,5 +19,42 @@ describe('Suite de testes na API Heroes', function() {
 
         assert.deepEqual(statusCode, 200)
         assert.ok(Array.isArray(dados))
+    })
+
+    it('Listar /herois - deve  retornar somente 10 registros', async () => {
+        const tamanho_limit = 3
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${tamanho_limit}`
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+
+        assert.deepEqual(statusCode, 200)
+        assert.ok(dados.length === tamanho_limit)
+    })
+
+    it('Listar /herois - deve retornar um erro com LIMIT incorreto', async () => {
+        const tamanho_limit = 'AEE'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=${tamanho_limit}`
+        })
+
+        assert.deepEqual(result.payload, 'Erro interno no servidor')
+    })
+
+    it('Listar /herois - deve  filtrar um item', async () => {
+        const NAME = 'Homem Aranha-1619702069116'
+        const result = await app.inject({
+            method: 'GET',
+            url: `/herois?skip=0&limit=1000&nome=${NAME}`
+        })
+
+        const dados = JSON.parse(result.payload)
+        const statusCode = result.statusCode
+        assert.deepEqual(statusCode, 200)
+        assert.deepEqual(dados[0].nome, NAME)
     })
 })
